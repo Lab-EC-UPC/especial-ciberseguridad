86e31a566a99b9ea012b9e836f1c02b3c296aef8
sayufu <lilianafuye@gmail.com>
2024-12-09 20:37:53 -0500

    fix: fixed teaser splash screen

--- src/pages/TeaserSplashScreen.tsx
+++ src/pages/TeaserSplashScreen.tsx
@@ -1,6 +1,6 @@
 export default function TeaserSplashScreen({ onSkip }: { onSkip: () => void }) {
     return (
-        <div className="relative h-screen w-screen overflow-hidden">
+        <div className="relative h-screen w-screen overflow-hidden bg-wallpaper">
             <div className="absolute top-4 left-4 z-10">
                 <img src="https://avatars.githubusercontent.com/u/189483635?s=200&v=4"
                      alt="Lab EC-Data UPC"
@@ -8,7 +8,7 @@ export default function TeaserSplashScreen({ onSkip }: { onSkip: () => void }) {
                 />
             </div>
             <video
-                className="absolute top-0 left-0 h-full w-full object-cover"
+                className="absolute top-0 left-0 h-full w-full object-cover p-2 md:p-8"
                 src="https://videos.pexels.com/video-files/6330778/6330778-hd_1920_1080_30fps.mp4"
                 autoPlay
                 controls
