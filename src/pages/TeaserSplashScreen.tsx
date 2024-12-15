23fb70187d7407932c22e31e245ebb0d27db9dbd
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-12-15 16:01:28 -0500

    Add files via upload

--- src/pages/TeaserSplashScreen.tsx
+++ src/pages/TeaserSplashScreen.tsx
@@ -19,7 +19,7 @@ export default function TeaserSplashScreen({ onSkip }: { onSkip: () => void }) {
                     className="w-screen h-screen object-cover"
                 >
                     <source
-                        src="https://okalphavideo.s3.us-east-2.amazonaws.com/makereign/idents/website/thumbnail-002.mp4"
+                        src="https://github.com/Lab-EC-UPC/assets/raw/refs/heads/main/especial-ciberseguridad/videos/ec-ciberseguridad-horizontal_webm_hd.webm"
                         type="video/mp4"
                     />
                     Your browser does not support HTML video.
@@ -27,6 +27,12 @@ export default function TeaserSplashScreen({ onSkip }: { onSkip: () => void }) {
             </div>
 
             <div className="flex gap-4 absolute top-2 right-4 z-10">
+                <button
+                    onClick={onSkip}
+                    className="button-green animate-scale-loop"
+                >
+                    Ver especial
+                </button>
                 <button
                     onClick={() => setIsMuted(!isMuted)}
                     className="rounded-full text-white bg-black bg-opacity-50 p-2"
@@ -44,15 +50,6 @@ export default function TeaserSplashScreen({ onSkip }: { onSkip: () => void }) {
                             className="h-8 w-8"
                         />
                     )}
-                </button>
-            </div>
-
-            <div className="flex gap-4 absolute bottom-8 right-4 z-10">
-                <button
-                    onClick={onSkip}
-                    className="button-green"
-                >
-                    Ver especial
                 </button>
             </div>
         </div>
