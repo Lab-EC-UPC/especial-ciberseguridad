e7da847dc3c3b9015a22713d0db517f1b5b3f309
Daniel Zapata <90068737+daniel7apata@users.noreply.github.com>
2024-12-15 10:26:39 -0500

    2 pages updated
    Opiniones sobre la ciberdelincuencia y Prevención y Denuncia

--- src/pages/OpinionesSobreLaCiberdelincuencia.tsx
+++ src/pages/OpinionesSobreLaCiberdelincuencia.tsx
@@ -1,17 +1,22 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import getCurrentTime from "../components/CurrentTime.ts";
 
-export default function OpinionesSobreLaCiberdelincuencia () {
+export default function OpinionesSobreLaCiberdelincuencia() {
     const [visibleElements, setVisibleElements] = useState(0);
 
     const elements = [
-        <div className="flex flex-col items-end animate-fade-in-fast">
-            <div className="chat-box right">
+        <div className="flex justify-center">
+        <h1 className="chat-title-box">
+            ¿Qué dicen los especialistas?
+        </h1>
+    </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
                 <p className="text-sm md:text-md">
-                    <span className="font-bold">Hola </span>
-                    Esto es un ejemplo
+                    <span className="font-bold">🔴 Especialista 1: Diego Oviedo </span>
                 </p>
                 <div className="flex items-center gap-1">
-                    <small className="chat-time">18:50</small>
+                    <small className="chat-time">{getCurrentTime()}</small>
                     <img
                         src="read-double-check-icon.svg"
                         alt="Double check icon"
@@ -20,6 +25,243 @@ export default function OpinionesSobreLaCiberdelincuencia () {
                 </div>
             </div>
         </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    Especialista en Ciberseguridad
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <img src="https://github.com/Lab-EC-UPC/assets/blob/main/especial-ciberseguridad/especialistas/Diego%20Oviedo.jpeg?raw=true" style={{ width: '200px', height: '200px', borderRadius: '50%' }}></img>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    “Cuando tú le preguntas a un delincuente ‘¿por qué el mundo de la ciberdelincuencia es atractivo para ti?’, lo primero que te va a decir, probablemente, es que en esta forma de delinquir uno no tiene que exponer la vida. Para un delincuente es un mundo donde puedes generar mucho impacto negativo con simplemente algunos clics”
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    <span className="font-bold">🔴 Especialista 2: Jose Carlos Vargas</span>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    Presidente de ISACA
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <img src="https://github.com/Lab-EC-UPC/assets/blob/main/especial-ciberseguridad/especialistas/Jose%20Carlos%20Vargas.jpeg?raw=true" style={{ width: '200px', height: '200px', borderRadius: '50%' }}></img>
+        </div>,
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    “Nunca antes el saber cómo protegerte en el mundo digital ha marcado tanto impacto en la calidad de vida de todas las personas. (…) En temas de ciberseguridad, ahora mismo hay un fenómeno que es mundial: la ausencia de perfiles profesionales para poder abordar los retos de la ciberseguridad. No hay profesionales”
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    <span className="font-bold">🔴 Especialista 3: Victor Ruiz </span>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    Especialista en Ciberseguridad
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <img src="https://github.com/Lab-EC-UPC/assets/blob/main/especial-ciberseguridad/especialistas/VictorRuiz_squared.jpg?raw=true" style={{ width: '200px', height: '200px', borderRadius: '50%' }}></img>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    “Es muy importante que se integre la ciberseguridad como parte del desarrollo económico. Recientemente, hablábamos con algunas empresas y les decíamos: ‘tú ya tienes que considerar en tu empresa tener un área de ciberseguridad que le dé seguridad tanto a sus colaboradores y clientes, así como tienes tu área legal, tu área de finanzas y tu área de recursos humanos”.
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+
+
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    <span className="font-bold">🔴 Especialista 4: Erick Iriarte</span>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    Especialista en Derecho Digital
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <img src="https://github.com/Lab-EC-UPC/assets/blob/main/especial-ciberseguridad/especialistas/Jose%20Carlos%20Vargas.jpeg?raw=true" style={{ width: '200px', height: '200px', borderRadius: '50%' }}></img>
+        </div>,
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    “En el Perú, no tenemos cultura de ciberseguridad ni protección de datos. Ni siquiera los niños en las escuelas aprenden cómo cuidar su información o la de sus familias. (...)El reglamento de protección de datos tiene más de una década y no se ajusta a las dinámicas actuales. Un nuevo reglamento es urgente para dar más herramientas a los ciudadanos"
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+
+
+
+
+
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    <span className="font-bold">🔴 Especialista 5: Jorge Zeballos</span>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                Gerente General de ESET Perú  
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <img src="https://github.com/Lab-EC-UPC/assets/blob/main/especial-ciberseguridad/especialistas/JorgeZeballos_squared.jpg?raw=true" style={{ width: '200px', height: '200px', borderRadius: '50%' }}></img>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                “Como ciudadanos, necesitamos que el tema de la protección de los datos personales esté mejor regulado. Mi dato no puede caminar libremente, ellos saben dónde trabajas, cuánto ganas, qué tarjetas usas, etc., y eso no está bien. Es parte de mi derecho fundamental, y está establecido hasta en la Constitución”               </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
     ];
 
 
--- src/pages/PrevencionYDenuncia.tsx
+++ src/pages/PrevencionYDenuncia.tsx
@@ -1,17 +1,155 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
+import getCurrentTime from "../components/CurrentTime.ts";
 
-export default function PrevencionYDenuncia () {
+export default function PrevencionYDenuncia() {
     const [visibleElements, setVisibleElements] = useState(0);
 
     const elements = [
+        <div className="flex justify-center">
+            <h1 className="chat-title-box">
+                ¿Cómo prevenir estos ciberdelitos?
+            </h1>
+        </div>,
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    Según un estudio de <span className="font-bold">Kaspersky 📊, aunque el 69% de los peruanos</span> se preocupa
+                    por revisar la <span className="font-bold">legitimidad de los sitios web</span> 🌐 antes de ingresar sus <span className="font-bold">datos
+                        personales</span> 🔐, el <span className="font-bold">77% reconoce que no sabe
+                            cómo identificar una página falsa.</span> ⚠️❌
+
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+        <div className="flex justify-center">
+            <h1 className="chat-title-box">
+                Canales y vías de denuncia
+            </h1>
+        </div>,
+
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    En esta sección podrás encontrar los <span className="font-bold">principales medios para
+                        denunciar ciberdelitos</span> en el Perú. En caso hayas sido
+                    <span className="font-bold">víctima de un ciberdelito</span> 🖥️🔒 o busques <span className="font-bold">reportar una actividad
+                        sospechosa</span> ⚠️, puedes ponerte en <span className="font-bold">contacto con las siguientes
+                            entidades responsables:</span> 📞📧.
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
         <div className="flex flex-col items-end animate-fade-in-fast">
             <div className="chat-box right">
                 <p className="text-sm md:text-md">
-                    <span className="font-bold">Hola </span>
-                    Esto es un ejemplo
+                    📞 <span className="font-bold">División de Investigación de Delitos de Alta Tecnología (DIVINDAT)</span> 💻🔍
                 </p>
                 <div className="flex items-center gap-1">
-                    <small className="chat-time">18:50</small>
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    Esta es la unidad especializada de la Policía Nacional del Perú (PNP)
+                    que atiende delitos informáticos.
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+        <div className="flex flex-col items-end animate-fade-in-fast">
+            <div className="chat-box right">
+                <p className="text-sm md:text-md">
+                    ☎️ Teléfonos: (01) 431-8898 o línea gratuita 1818<br></br>
+                    📧 Correo: divindat.depcpi@policia.gob.pe<br></br>
+                    📍 Dirección: Piso 9, Dirección de Investigación Criminal (DIRINCRI), Av. España 323, Cercado de Lima.
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    📞 <span className="font-bold">Ministerio Público - Fiscalía de la Nación ⚖️💻</span>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                    📞 En su portal puedes <span className="font-bold">presentar
+                        denuncias</span> sobre cualquier tipo de delito, incluidos los
+                    ciberdelitos. <br></br>🌐✅Es necesario <span className="font-bold">identificarte con tu DNI
+                        🆔 y proporcionar pruebas claras 📂.</span>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
+                    <img
+                        src="read-double-check-icon.svg"
+                        alt="Double check icon"
+                        className="h-4 w-4 md:h-6 md:w-6"
+                    />
+                </div>
+            </div>
+        </div>,
+
+
+        <div className="flex flex-col items-start animate-fade-in-fast">
+            <div className="chat-box left">
+                <p className="text-sm md:text-md">
+                <span className="font-bold">🔗 Enlace:</span> <a href="https://portal.mpfn.gob.pe/denuncias-en-linea" style={{color: "#3497f9", textDecoration: "underline"}}>Denuncias en línea</a>
+                </p>
+                <div className="flex items-center gap-1">
+                    <small className="chat-time">{getCurrentTime()}</small>
                     <img
                         src="read-double-check-icon.svg"
                         alt="Double check icon"
@@ -34,6 +172,7 @@ export default function PrevencionYDenuncia () {
 
     return (
         <>
+
             <div>
                 {elements.slice(0, visibleElements).map((element) => element)}
             </div>
